fe6f2dbcba0e71dd26f2c598313675e9243a39d1
Auden Robertson <audenrobertson@gmail.com>
2018-06-08 03:23:35 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete :name
-    redirect_to controller: 'application', action: 'hello'  
+    redirect_to controller: 'application', action: 'hello'
   end
 end
