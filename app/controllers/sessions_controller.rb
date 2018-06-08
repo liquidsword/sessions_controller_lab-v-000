9b775c48a8085ed3c8c962562019ed095a1f60dd
Auden Robertson <audenrobertson@gmail.com>
2018-06-08 03:06:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
-  def create(name)
-    if :name = nil
-      redirect_to :login
+  def create
+    return redirect_to(controller: 'sessions', action: 'new')
+    if !params[:name] || params[:name].empty?
+      session[:name] = params[:name]
+      redirect_to controller: 'application', action: 'hello'
 
     end
 
